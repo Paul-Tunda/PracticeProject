3fab59cb8d759982e79dbac0857aca58f457daa2
Paul-Tunda <paultunda35@gmail.com>
2025-09-30 10:52:51 -0400

    feat: Add InvalidTransactionException to handle transfers

---
+++ exceptions/InvalidTransactionException.java
@@ -0,0 +1,11 @@
+package lesson19customexcptions.PracticeProject.exceptions;
+
+public class InvalidTransactionException extends Exception {
+
+    public InvalidTransactionException(String message) {
+
+        super(message);
+
+    }
+
+}
