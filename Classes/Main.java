e8babaf437bc7fc4a9001133a208b1a71c62fc8d
Paul-Tunda <paultunda35@gmail.com>
2025-09-30 04:03:51 -0400

    perform initial deposit

--- Classes/Main.java
+++ Classes/Main.java
@@ -1,5 +1,9 @@
 package lesson19customexcptions.PracticeProject.Classes;
 
+import lesson19customexcptions.PracticeProject.exceptions.InvalidDepositException;
+
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -7,6 +11,22 @@ public class Main {
         BankAccount bankAccount = new BankAccount();
 
         System.out.println("Account balance: $" + bankAccount.getBalance());
+
+        double initialDeposit;
+
+        try(Scanner sc = new Scanner(System.in)) {
+
+            System.out.println("Enter deposit amount: ");
+            initialDeposit = sc.nextDouble();
+            sc.nextLine();
+
+            bankAccount.deposit(initialDeposit);
+
+        }catch (InvalidDepositException e) {
+
+            System.err.println("Error occurred!!!\n" + e.getMessage());
+
+        }
 
     }
 
