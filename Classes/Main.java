240dbabde71dec0a58f48873f634679d6f947f7f
Paul-Tunda <paultunda35@gmail.com>
2025-09-30 03:56:23 -0400

    feat: add Main.java to simulation account transactions

---
+++ Classes/Main.java
@@ -0,0 +1,13 @@
+package lesson19customexcptions.PracticeProject.Classes;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        BankAccount bankAccount = new BankAccount();
+
+        System.out.println("Account balance: $" + bankAccount.getBalance());
+
+    }
+
+}
